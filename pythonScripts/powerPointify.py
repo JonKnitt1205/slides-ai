997d0611e66c62aa7dd28d2ceff7d88db12d4fc8
JonKnitt1205 <106881524+JonKnitt1205@users.noreply.github.com>
2023-06-07 13:44:29 -0400

    Update powerPointify.py

--- pythonScripts/powerPointify.py
+++ pythonScripts/powerPointify.py
@@ -8,22 +8,21 @@ import sys
 # this main script handles all steps of slide creation
 if __name__ == "__main__": 
 
-    # read api key and orginization from a text file
+    # read api key from a text file
     with open("keyFile.txt", "r") as f:
         key = f.readline().replace("\n", "")
-        orginization = f.readline().replace("\n", "")
 
     # theme sent in from arguments
     theme = sys.argv[1]
     
     # step one, create a text string from a chat ai
-    textResponse = chatBot(key, orginization).getChatResponse(theme, 1024, 0)
+    textResponse = chatBot(key).getChatResponse(theme, 1024, 0)
     
     # step two, make that text string into a more useful object format
     dictVersionOfData = dictMaker().createDict(textResponse)
 
     # step three, generate the pictures
-    imageBinarys = clipArtBot(key, orginization).createImages(dictVersionOfData)
+    imageBinarys = clipArtBot(key).createImages(dictVersionOfData)
 
     # step four, create the presentation
     tiredStudent(dictVersionOfData, imageBinarys).createPresentation()
